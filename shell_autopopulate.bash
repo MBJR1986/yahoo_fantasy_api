b951ab870e20a724ddf3ac029978f5c077c180c5
Mark Burghart <m.a.burghart@gmail.com>
2020-10-18 20:47:32 -0500

    Regular auto-commit at 20:47:32 on 18/10/2020

---
+++ shell_autopopulate.bash
@@ -0,0 +1,9 @@
+#!/bin/sh
+cd ~/nfl-fantasy-football_Group1/
+
+git add --all
+timestamp() {
+  date +"at %H:%M:%S on %d/%m/%Y"
+}
+git commit -am "Regular auto-commit $(timestamp)"
+git push origin master
