56e24a9130fae67ba95988ca3be8729b8bca273c
Mark Burghart <m.a.burghart@gmail.com>
2020-10-18 22:40:44 -0500

    Regular auto-commit at 22:40:44 on 18/10/2020

--- processing.bat
+++ processing.bat
@@ -1,4 +1,4 @@
-"C:\Users\mabur\AppData\Local\Microsoft\WindowsApps\python3.exe" "C:\Users\mabur\nfl-fantasy-football_Group1\fantasy_stats.py"]
+"C:\Users\mabur\anaconda3\python.exe" "C:\Users\mabur\nfl-fantasy-football_Group1\fantasy_stats.py"]
 pause
-"C:\Users\mabur\AppData\Local\Microsoft\WindowsApps\python3.exe" "C:\Users\mabur\nfl-fantasy-football_Group1\Data Analysis\Data Transformations.py"
+"C:\Users\mabur\anaconda3\python.exe" "C:\Users\mabur\nfl-fantasy-football_Group1\Data Analysis\Data Transformations.py"
 pause
